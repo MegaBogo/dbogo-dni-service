f4380accea5fdf28a4529ce4c1488a63d1be5f46
Koby <devbogo@gmail.com>
2019-03-11 22:04:54 +0900

    MybatisMemberDaoClass add

--- src/main/java/com/dni/web/dao/mybatis/MybatisMemberDao.java
+++ src/main/java/com/dni/web/dao/mybatis/MybatisMemberDao.java
@@ -2,65 +2,71 @@ package com.dni.web.dao.mybatis;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.dni.web.dao.MemberDao;
 import com.dni.web.entity.Member;
 
+@Repository
 public class MybatisMemberDao implements MemberDao{
 
-	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@Override
-	public int insert(Member users) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insert(Member member) {
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.insert(member);
 	}
 
 	@Override
-	public int update(Member users) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int update(Member member) {
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.update(member);
 	}
 
 	@Override
 	public int delete(String id) {
-		// TODO Auto-generated method stub
-		return 0;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.delete(id);
 	}
 
 	@Override
 	public Member get(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.get(id);
 	}
 
 	@Override
 	public List<Member> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.getList();
 	}
 
 	@Override
 	public List<Member> getList(int page) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.getList(page);
 	}
 
 	@Override
 	public List<Member> getList(String field, String name) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.getList(field, name);
 	}
 
 	@Override
 	public List<Member> getList(String field, String name, int page) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.getList(field, name, page);
 	}
 
 	@Override
 	public Member getByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		return memberDao.getByEmail(email);
 	}
 
 }
