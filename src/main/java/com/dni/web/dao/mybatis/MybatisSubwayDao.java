a49d5a52a54ea4107cf38c91c8e2b59098851f76
Koby <devbogo@gmail.com>
2019-03-11 22:05:03 +0900

    MybatisSubwayDao.class add

--- src/main/java/com/dni/web/dao/mybatis/MybatisSubwayDao.java
+++ src/main/java/com/dni/web/dao/mybatis/MybatisSubwayDao.java
@@ -2,39 +2,47 @@ package com.dni.web.dao.mybatis;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.dni.web.dao.SubwayDao;
 import com.dni.web.entity.Subway;
 
+@Repository
 public class MybatisSubwayDao implements SubwayDao {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@Override
 	public int insert(Subway subway) {
-		// TODO Auto-generated method stub
-		return 0;
+		SubwayDao subwayDao = sqlSession.getMapper(SubwayDao.class);
+		return subwayDao.insert(subway);
 	}
 
 	@Override
 	public int update(Subway subway) {
-		// TODO Auto-generated method stub
-		return 0;
+		SubwayDao subwayDao = sqlSession.getMapper(SubwayDao.class);
+		return subwayDao.insert(subway);
 	}
 
 	@Override
 	public int delete(int id) {
-		// TODO Auto-generated method stub
-		return 0;
+		SubwayDao subwayDao = sqlSession.getMapper(SubwayDao.class);
+		return subwayDao.delete(id);
 	}
 
 	@Override
 	public Subway get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		SubwayDao subwayDao = sqlSession.getMapper(SubwayDao.class);
+		return subwayDao.get(id);
 	}
 
 	@Override
 	public List<Subway> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		SubwayDao subwayDao = sqlSession.getMapper(SubwayDao.class);
+		return subwayDao.getList();
 	}
 
 }
