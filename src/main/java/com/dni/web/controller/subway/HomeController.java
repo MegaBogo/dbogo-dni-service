1b8eaba55f655768728022fc400145f298a1685e
Koby <devbogo@gmail.com>
2019-03-12 11:16:06 +0900

    Controller

--- src/main/java/com/dni/web/controller/subway/HomeController.java
+++ src/main/java/com/dni/web/controller/subway/HomeController.java
@@ -3,8 +3,8 @@ package com.dni.web.controller.subway;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/subway")
-public class SubwayController {
+@Controller("subwayHomeController")
+@RequestMapping("/subway/")
+public class HomeController {
 
 }
