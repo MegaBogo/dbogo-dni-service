6a28e7000692963ac07ccf53911600a2f1472c98
Koby <devbogo@gmail.com>
2019-03-14 10:44:19 +0900

    SubwayService HomeController

--- src/main/java/com/dni/web/controller/subway/HomeController.java
+++ src/main/java/com/dni/web/controller/subway/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dni.web.entity.Subway;
@@ -14,17 +15,32 @@ import com.dni.web.service.SubwayService;
 import com.mysql.cj.Session;
 
 @Controller("subwayHomeController")
-@RequestMapping("/subway")
+@RequestMapping("/subway/")
 public class HomeController {
 	
 	@Autowired
 	private SubwayService subwayService;
 	
-	 @GetMapping("/")
+	 @GetMapping("")
 	 public String index(Model model) {
 		 return "subway.index";
 	 }
+
+	 @GetMapping("reg")
+	 public String reg() {
+		 return "subway.reg";
+	 }
 	 
+	 @PostMapping("/reg")
+	 public String reg(Principal principal, Subway subway) {
+		String regUserId = principal.getName();
+		subway.setRegUserId(regUserId);
+		
+		subwayService.addSubway(subway);
+		
+		return "redirect:../";
+	 }
+	 	 
 	 @GetMapping("/{id}")
 	 public String index(Principal principal, @PathVariable("id") int id, Model model) {
 		 //Spring Security 현재 세션 사용자의 정보 
