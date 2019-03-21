1318bea2047195a871f5a01b918d81e0b8e3cc08
Koby <devbogo@gmail.com>
2019-03-22 00:45:56 +0900

    AjaxController

--- src/main/java/com/dni/web/controller/AjaxController.java
+++ src/main/java/com/dni/web/controller/AjaxController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dni.web.entity.study.ChagyebuVO;
 
+
 @Controller
 public class AjaxController {
 	/**
@@ -26,15 +27,25 @@ public class AjaxController {
 //		return chagyebuService.selectChagyebuList(vo);
 //	}
 	
-	@GetMapping("/ajax/search-get")
-	public @ResponseBody Map<String, Object> searchGet(ChagyebuVO vo) throws Exception {
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("member", vo.getMember());
-		map.put("type", vo.getType());
-		
-		//return chagyebuService.selectChagyebuList(vo);
-		return map;
-	}
-	
+//	@GetMapping("/ajax/search-get")
+//	public @ResponseBody String searchGet(ChagyebuVO vo) throws Exception {
+//		String[] member = {"한화","삼성"};
+//		String[] type = {"1","2","3"};
+//		vo.setMember(member);
+//		vo.setType(type);
+//		
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("member", vo.getMember());
+//		map.put("type", vo.getType());
+//		
+//		//return chagyebuService.selectChagyebuList(vo);
+//		
+//		ObjectMapper objectMapper = new ObjectMapper();
+//	    String json = objectMapper.writeValueAsString(map);
+//	    
+//	    System.out.println(json);
+//	    
+//		return json;
+//	}
+//	
 }
